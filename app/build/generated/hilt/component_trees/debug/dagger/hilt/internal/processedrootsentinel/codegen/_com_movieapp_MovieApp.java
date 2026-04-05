ef5695038d47ebff7e5dbaeb28d38ba1e125da75
Vikas <dhimanv209@gmail.com>
2026-04-05 21:14:25 +0530

    chore: remove build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_movieapp_MovieApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.movieapp.MovieApp"
-)
-public final class _com_movieapp_MovieApp {
-}
